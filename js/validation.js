fff92c41a49f8a1c5f5ce91c32fdc2c43958b41b
Arseniy <arseniy.cimbris@gmail.com>
2023-09-18 19:59:21 +0300

    made a test form

---
+++ js/validation.js
@@ -0,0 +1,50 @@
+$(function () {
+
+	// находим форму, div с сообщением об успехе
+	const form = $('#form');
+	const answer = $('#answer');
+	
+
+	// функция валидаций(заполненности полей)
+	form.validate({
+
+		// находим инпуты в форме и устанавливаем им правила(все должны быть заполнены)
+	  rules: {
+		 email: { required: true, email: true },
+		 message: { required: true }
+	  },
+
+	//   сообщения при несоблюдении правил
+	  messages: {
+		 email: { required: 'Введите email', email: 'Отсутствует символ @' },
+		 message: { required: 'Поле не может быть пустым' }
+	  },
+
+	//   вызываем асинхронный запрос(асинхронный тк страница продолжает работать)
+	  submitHandler: ajaxFormSubmit
+	});
+ 
+
+	// функция асинхронного запроса
+	function ajaxFormSubmit() {
+
+		// собираем все данные с формы
+	  const string = form.serialize();
+ 
+
+	//   асинхронный запрос(метод post отвечает за отправку, url указывает путь на php документ, который будет обрабатывать данные)
+	// в свойстве data мы передаем в php документ данные из формы
+	// success отвечает за анимацию если php документ успешно отправил данные
+	  $.ajax({
+		 type: 'POST',
+		 url: './php/mail.php',
+		 data: string,
+		 success: function (html) {
+			form.slideUp(800);
+			answer.html(html);
+		 }
+	  });
+//  остановка функции
+	  return false;
+	}
+ });
---
+++ php/mail.php
@@ -0,0 +1,54 @@
+<?php
+
+//Script Foreach
+$c = true;
+
+// For POST method only!
+
+$project_name = "arseniy.com";
+$admin_email = "arseniy.cimbris@gmail.com";
+// почтовый ящик, привязанный к хостингу
+$email_from = "pivnenkowebdev@pivnenko-anton.pro";
+$message = "";
+
+// Serialize form fields - that filled-in by User
+foreach ($_POST as $key => $value) {
+	if ($value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" && $key != "email_from") {
+		$message .= "
+		" . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+	</tr>
+	";
+	}
+}
+
+// Create message text for sending on email
+$message = "<table style='width: 100%;'>$message</table>";
+
+
+
+// Adjusting text encoding
+function adopt($text)
+{
+	return '=?UTF-8?B?' . base64_encode($text) . '?=';
+}
+
+$form_subject = 'Заявка с сайта Portfolio';
+
+// Preparing header
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+	"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+	'From: ' . adopt($project_name) . ' <' . $email_from . '>' . PHP_EOL .
+	'Reply-To: ' . $admin_email . '' . PHP_EOL;
+
+// Sending email to admin
+mail($admin_email, $form_subject, $message, $headers, $email_from);
+
+
+
+echo "<div class='success'>
+		<h2>Заявка принята!<br>
+		Мы свяжемся с&nbsp;Вами в&nbsp;ближайшее время!
+		</h2>
+	  </div> ";
